4d39b90da01509ed28e4a285a00806078780935c
amitspk <amit11081994@gmail.com>
2025-07-06 19:39:02 +0530

    Updated product tags

--- fake_db.py
+++ fake_db.py
@@ -1,10 +1,10 @@
 # fake_db.py
 FAKE_DB = [
-    {"name": "TrailRunner 1000", "tags": ["running", "shoes", "grip", "affordable"], "price": 79.99},
-    {"name": "SpeedX Elite", "tags": ["running", "shoes", "lightweight"], "price": 129.99},
-    {"name": "BudgetFit Pro", "tags": ["affordable" "shoes", "walking"], "price": 49.99},
-    {"name": "GripMax Trek", "tags": ["grip", "hiking",  "shoes","durable"], "price": 109.99},
-    {"name": "GripMax Trek V2", "tags": ["grip", "hiking", "shoes", "durable"], "price": 119.99},
+    {"name": "TrailRunner 1000", "tags": ["running", "grip", "affordable"], "price": 79.99},
+    {"name": "SpeedX Elite", "tags": ["running", "lightweight"], "price": 129.99},
+    {"name": "BudgetFit Pro", "tags": ["affordable", "walking"], "price": 49.99},
+    {"name": "GripMax Trek", "tags": ["grip", "hiking" ,"durable"], "price": 109.99},
+    {"name": "GripMax Trek V2", "tags": ["grip", "hiking", "durable"], "price": 119.99},
 ]
 
 # def fetch_from_db(tags):
